e45f00ea7b6c70eeb8b8cb7fc53a664942a0f934
nhardbalansag <nhardbalansag@gmail.com>
2021-01-04 04:52:52 +0800

    adding react-native-image-crop-picker library

--- src/screens/sendDocument.js
+++ src/screens/sendDocument.js
@@ -5,6 +5,8 @@ import {
     Text, 
 } from 'react-native';
 
+import ImagePicker from 'react-native-image-crop-picker';
+
 import {
     styles, 
     colors
@@ -14,9 +16,27 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const SendDocument = ({navigation}) =>{
 
+    const openCamera = async () =>{
+        try {
+            await ImagePicker.openCamera({
+                width: 300,
+                height: 400,
+                cropping: true,
+              }).then(image => {
+                console.log(image);
+              });
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     return(
         <View style={[{flex:1, flexDirection:'column', justifyContent:'space-between', backgroundColor:colors.lightColor}]}>
-            
+            <TouchableOpacity onPress={() =>openCamera()} style={styles.GeneralButton}>
+                <View>
+                    <Text>open camera</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
