c5c6be9986c20bd4e54d3c3c9323f4a8a52a373f
nhardbalansag <nhardbalansag@gmail.com>
2021-01-05 05:26:49 +0800

    setting up the device camera for document submition

--- src/screens/sendDocument.js
+++ src/screens/sendDocument.js
@@ -5,6 +5,8 @@ import {
     Text, 
 } from 'react-native';
 
+import ImagePicker from 'react-native-image-crop-picker';
+
 import {
     styles, 
     colors
@@ -14,9 +16,41 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const SendDocument = ({navigation}) =>{
 
+    const openDeviceCamera = async () =>{
+        try {
+            await ImagePicker.openCamera({
+                width: 300,
+                height: 400,
+                cropping: true,
+              }).then(image => {
+                console.log(image);
+              });
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     return(
-        <View style={[{flex:1, flexDirection:'column', justifyContent:'space-between', backgroundColor:colors.lightColor}]}>
-            
+        <View style={[{flex:1, flexDirection:'column', justifyContent:'space-around', backgroundColor:colors.lightColor}]}>
+            <View style={{ marginHorizontal:20}}>
+                <View>
+                    <Text style={{ fontSize:15 }}>Upload Document</Text>
+                    <View>
+                        <Text style={{ color:colors.disableColor }}>
+                            <Text style={{ fontWeight:'bold' }}>Note: </Text>
+                            <Text>Make sure the photo is clear and the whole document is seen in the shot. Don't forget to check its expiry date!</Text>
+                        </Text>
+                    </View>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={() =>openDeviceCamera()} style={[{backgroundColor:colors.disableColor}]}>
+                        <View style={{ flexDirection:'row', justifyContent:'center', alignItems:'center', paddingVertical:10 }}>
+                            <Icon name="photo-camera" size={30} color={'black'}/>
+                            <Text style={{ color:colors.lightColor }}>Tap to take Document photo</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 }
